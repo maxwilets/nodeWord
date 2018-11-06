f31ef128b9207246dac12a3a76c18f4686ddee83
Max Wilets <mwilet@gmail.com>
2018-11-06 12:11:24 -0700

    started word.js

---
+++ letter.js
@@ -0,0 +1,25 @@
+guess = process.argv[2]
+
+var Letter = function (letter){
+    
+    this.letter = letter   //this is the character for the letter
+
+    this.hasGuessed = false; //tells the computer if the guessed letter is correct or not
+
+    this.whenGuessed = function(){     
+    //if the letter guessed is wrong it will fill with _ if not it will do the guess
+    //this function will be looped in the word.js 
+        if (this.hasGuessed === false){
+            return "_"
+        }
+        return this.letter
+    }
+    //checks if the guess matches any of the letters in the word
+    this.checkLetter = function(){
+        if (guess === this.letter){
+            this.hasGuessed = true
+        }
+    }
+}
+
+module.exports = Letter 
---
+++ word.js
@@ -0,0 +1,22 @@
+Letter = require('./letter')
+var leter = new Letter;
+var wordAr = [];
+var word;
+
+
+function Word(word) {
+    this.word = word;
+    this.wordAr = [];
+    this.letterAr[]
+    //this breaks up the letters of the word and puts them into an array
+    this.letters = function(){
+        //splits the word into seperate objects
+        this.wordAr = this.word.split("");
+        for(i = 0; i < wordAr.length; i ++ ){
+            //the new letter is equal to one of the letters in the word and then
+            //put through the letter constructor
+            var newLetter = leter(this.wordAr[i]);
+            this.letterAr.push(newLetter)
+        }
+    }
+}
