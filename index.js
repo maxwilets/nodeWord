675c3b3ea5f33a5649421657a0bff2134246072f
Max Wilets <mwilets@gmail.com>
2018-11-08 21:08:09 -0700

    polished up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//var Word = require('./word.js');
+var Word = require('./word.js');
 var inquire = require('inquirer')
-var guessesWrong = 2
+var guessesWrong = 0
 
 //var guess = process.argv[2]
 //Array of words that will be randomly chosen for the game
@@ -23,100 +23,110 @@ var hangmanDisplay = ["\n\n\n\n\n\n\n-------------", "\n |\n |\n |\n |\n |\n |\n
     "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        | |\n |\n-------------"
 ]
 
-
-//console.log(ffArr[randomNum])
 var game = {
-
-    
+    wordBank: ffArr,
     guessesWrong: 2,
-    guessedAr: ['2'],
+    guessedLetters: [],
     display: 0,
     currentWord: null,
-   // currentWord: words,
-    startGame: () =>{
-             
+   
+    startGame: function() {
+        if (this.guessedLetters.length > 0) {
+            this.guessedLetters = []
+        }
+        inquire.prompt({
+            name: 'start',
+            type: 'confirm',
+            message: 'Ready to start?'
+        }).then(function(data){
+            if(data.start){
+                game.newGame();
+            }
+            else{
+                console.log('Just press up + enter when ready')
+            }
+        })
+    },
+    newGame: function() {
         console.reset = () => {
-         return process.stdout.write('\033c');
-
-           
+            return process.stdout.write('\033c');
         }
-        
-       
-        guessedAr = []  
-        this.question= () => {
-            inquire.prompt({
-                name: 'guess',
-                type: 'input',
-                message: 'Guess a letter'
-            }).then(function(data){
-               thisGuess = (data.guess);
-               var already = false;
-               console.reset();
-
-               for(i = 0; i < this.guessedAr.length; x ++) {
-               
-                    if (thisGuess == this.guessedAr[i]){
-                       console.log("*** You've already guessed " + data.guess + '***');
-                       already = true
-                       this.question()
-                   }
-               }
-               if (already === false) {
-                   console.log(this.currentWord + 'yes')
-                   this.guessedAr.push(thisGuess);
-                  // var inLetterAr = this.currentWord.checkLetter1(thisGuess);
-                   if (this.currentWord.checkLetter1(thisGuess) == 0) {
-                       console.reset();
-                       console.log("\n*******          *******\n");
-                       guessesWrong ++;
-                       this.display ++;
-                       console.log(hangmanDisplay[guessesWrong])
-                       console.log('\n-_-_-_-_-_-_-_-_-_-_-_-_-')
-                       console.log('\n Letters Guessed: ' + this.guessedAr)
-
-                   }
-                   else if(this.currentWord.allFound() == true) {
-                       console.reset();
-                       console.log('\n*******          *******\n')
-                       console.log('Thats right, you gessed it! \n You Won!')
-                       process.exit()
-                   }
-                   else if (this.display == 0){
-                       console.reset();
-                       console.log('\n*******          *******\n')
-                       console.log('Guesses Remaining: ' + (12 - guessesWrong))
-                       console.log('\n*******          *******\n')
-                       console.log(this.currentWord.wordRend())
-                       console.log("Letters guessed: " + this.guessedAr)
-                   }
-                   else{
-                       console.reset();
-                       console.log('\n*******          *******\n')
-                       console.log('Correct \n\n*******          *******\n');
-                       console.log(hangmanDisplay[guessesWrong]);
-                       console.log(this.currentWord.wordRend);
-                       console.log('\n-_-_-_-_-_-_-_-_-_-_-_-_-')
-                       console.log("Letters guessed: " + this.guessedAr)
-                   }
-               }
-            })
-         }
-         guessesWrong = 2
-         if(guessesWrong == 2){
+        if(this.guessesWrong == 2) {
             console.reset();
-            this.randomNum= Math.floor(Math.random() * 64);
-            this.words= ffArr[this.randomNum];
-            console.log('\n Welcome to Console Hangman! \n The category is Final Fantasy Characters \n-_-_-_-_=_-_-_=_-_-_=_-_-_=_-');
-            console.log(this.guessedAr)
-            currentWord = new Word (this.words);
-            currentWord.letters();
-            console.log(hangmanDisplay[guessesWrong] + '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' )
-            console.log(currentWord.wordRend() + '\n')
-            this.question()
-            
+            console.log('\n Welcome to Console Hangman! \n The category is Final Fantasy Characters \n-_-_-_-_=_-_-_=_-_-_=_-_-_=_-')
+            game.randomNum= Math.floor(Math.random() * 64);
+            game.currentWord= new Word(ffArr[game.randomNum]);
+            game.currentWord.letters();
+            console.log(hangmanDisplay[guessesWrong])
+            console.log(game.currentWord.wordRend())
+            console.log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            game.gamePrompt()
         }
-        
+    },
+    gamePrompt: () => {
+        inquire.prompt({
+            name: 'guess',
+            type: 'input',
+            message: 'Guess a letter'
+
+        }).then(function(data){
+            var thisGuess = data.guess;
+            var guessedBoo = false;
+            console.reset();
+            for (i = 0; i < game.guessedLetters.length; i ++) {
+                if(thisGuess == game.guessedLetters[i]){
+                    guessedBoo = true;
+                    console.reset();
+                    console.log('You have already guessed that letter');
+                    game.gamePrompt();
+                }
+            }
+            if(guessedBoo == false) {
+                game.guessedLetters.push(thisGuess);
+               
+                var inTheWord = game.currentWord.checkLetter1(thisGuess);
+                if (inTheWord === 0){
+                  //  console.reset();
+                    guessesWrong ++
+                    if (guessesWrong === 10) {
+                        console.log("Game over, the correct word was " + game.currentWord.word);
+                        
+                        process.exit()
+                    }
+                   // console.log(game.currentWord)
+                    console.log('\n*******          *******\n\n');
+                    console.log('\n'+ hangmanDisplay[guessesWrong])
+                    console.log('That letter is not in the word');
+                    console.log('\n guesses remaining: ' + (10-guessesWrong))
+                    console.log('\nLetters Guessed: ' + game.guessedLetters)
+                    console.log('\n' +game.currentWord.wordRend())
+                    game.gamePrompt()
+                }
+                else if(game.currentWord.allFound() == true){
+                    console.reset();
+                    console.log('\n*******          *******\n\n');
+                    console.log(game.currentWord.wordRend());
+                    console.log("You got the word right!");
+                    console.log('\n*******          *******\n\n')
+                    process.exit()
+                }
+                
+                else{
+                    console.reset();
+                    console.log('\n*******          *******\n\n');
+                    //console.log(hangmanDisplay[guessesWrong]);
+                    console.log(hangmanDisplay[guessesWrong])
+                    console.log('\n\n' + game.currentWord.wordRend())
+                    console.log('\n\nLetters guessed: ' + game.guessedLetters )
+                    console.log('\n Correct \n');
+                    game.gamePrompt()
+                    
+                }
+            }
+        })
     }
+   
+
 
 }
 game.startGame()
